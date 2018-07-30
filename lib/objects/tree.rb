155b89d0535e991478a35c2065503de181404262
Cyberarm <matthewlikesrobots@gmail.com>
2018-07-30 12:56:29 -0500

    Added distance for tree

--- lib/objects/tree.rb
+++ lib/objects/tree.rb
@@ -2,7 +2,7 @@ class IMICFPS
   class Tree < GameObject
     def setup
       bind_model(ModelLoader.new(type: :obj, file_path: "objects/tree.obj", game_object: self))
-      vert = @terrain.find_nearest_vertex(self)
+      vert = @terrain.find_nearest_vertex(self, 4.5)
       self.x = vert.x
       self.y = vert.y
       self.z = vert.z
