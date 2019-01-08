2f0cb47ed83c97ed2435568c727549f6f624ae08
Cyberarm <matthewlikesrobots@gmail.com>
2019-01-07 19:37:46 -0600

    Improved Linux libGL library loader

--- i-mic-fps.rb
+++ i-mic-fps.rb
@@ -14,12 +14,23 @@ when :OPENGL_PLATFORM_MACOSX
   OpenGL.load_lib("libGL.dylib", "/System/Library/Frameworks/OpenGL.framework/Libraries")
   GLU.load_lib("libGLU.dylib", "/System/Library/Frameworks/OpenGL.framework/Libraries")
 when :OPENGL_PLATFORM_LINUX
-  begin # Ubuntu (Debian)
-    OpenGL.load_lib("libGL.so", "/usr/lib/x86_64-linux-gnu")
-    GLU.load_lib("libGLU.so", "/usr/lib/x86_64-linux-gnu")
-  rescue # Manjaro (ARCH)
-    OpenGL.load_lib("libGL.so", "/usr/lib")
-    GLU.load_lib("libGLU.so", "/usr/lib")
+  gl_library_path = nil
+
+  if File.exists?("/usr/lib/x86_64-linux-gnu/libGL.so") # Ubuntu (Debian)
+    gl_library_path = "/usr/lib/x86_64-linux-gnu"
+
+  elsif File.exists?("/usr/lib/libGL.so") # Manjaro (ARCH)
+    gl_library_path = "/usr/lib"
+
+  elsif File.exists?("/usr/lib/arm-linux-gnueabihf/libGL.so") # Raspbian (ARM/Raspberry Pi)
+    gl_library_path = "/usr/lib/arm-linux-gnueabihf"
+  end
+
+  if gl_library_path
+    OpenGL.load_lib("libGL.so", gl_library_path)
+    GLU.load_lib("libGLU.so", gl_library_path)
+  else
+    raise RuntimeError, "Couldn't find GL libraries"
   end
 else
   raise RuntimeError, "Unsupported platform."
