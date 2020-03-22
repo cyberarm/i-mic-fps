3d8666192178598f0781f9b3e8e267b1a2a00972
Cyberarm <matthewlikesrobots@gmail.com>
2020-03-22 18:13:41 -0500

    Added notice on main menu if opengl version < 3.3

--- lib/ui/menus/main_menu.rb
+++ lib/ui/menus/main_menu.rb
@@ -20,11 +20,33 @@ class IMICFPS
         window.close
       end
 
+      gl_version = glGetString(GL_VERSION).to_s
+      major, minor = gl_version.split(" ").first.split(".").map { |v| v.to_i }
+      unless major >= 3 && minor >= 3
+message =
+"<b><c=f00>[Notice]</c></b> Your computer is reporting support for <b><c=f50>OpenGL #{major}.#{minor}</c></b>,
+however <b><c=5f5>OpenGL 3.3 or higher is required.</c></b>
+
+Fallback <b>immediate mode renderer</b> will be used."
+
+linux_mesa_message =
+"
+
+(Linux Only) For MESA based drivers append <b>--mesa-override</b>
+as a commandline argument to override reported version."
+        message += linux_mesa_message if RUBY_PLATFORM =~ /linux/ && gl_version.downcase.include?(" mesa ")
+        @old_gl_warning = Gosu::Image.from_markup(message, 24, align: :center)
+      end
+
       @text = CyberarmEngine::Text.new("<b>#{IMICFPS::NAME}</b> v#{IMICFPS::VERSION} (#{IMICFPS::RELEASE_NAME})")
     end
 
     def draw
       super
+
+      if @old_gl_warning
+        @old_gl_warning.draw(window.width / 2 - @old_gl_warning.width / 2, window.height - (@old_gl_warning.height + 10), Float::INFINITY)
+      end
 
       @text.draw
       @text.x = window.width - (@text.width + 10)
